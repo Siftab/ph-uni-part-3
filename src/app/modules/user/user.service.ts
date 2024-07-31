6143173da66327f3724b072a108e5837b88a27e1
Siftab <siiffuuu@gmail.com>
2024-07-31 11:05:57 +0600

    transection and rollback

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import config from '../../config';
 import { TStudent } from '../student/student.interface';
 import { Student } from '../student/student.model';
@@ -21,21 +22,48 @@ const createStudentIntoDB = async (password: string, payload: TStudent) => {
     payload.admissionSemester,
   );
 
-  //set  generated id
-  userData.id = await generateStudentId(admissionSemester);
+  const session =await mongoose.startSession()
 
-  // create a user
-  const newUser = await User.create(userData);
+try {
 
-  //create a student
-  if (Object.keys(newUser).length) {
-    // set id , _id as user
-    payload.id = newUser.id;
-    payload.user = newUser._id; //reference _id
+    //set  generated id
+    userData.id = await generateStudentId(admissionSemester);
 
-    const newStudent = await Student.create(payload);
-    return newStudent;
-  }
+    // create a user
+    const newUser = await User.create([userData],{session});
+  
+    //create a student
+    if (!newUser.length) {
+      throw new Error("failed to create a user ")
+    }
+      // set id , _id as user
+      payload.id = newUser[0].id;
+      payload.user = newUser[0]._id; //reference _id
+
+
+  
+      const newStudent = await Student.create([payload],{session});
+
+
+      if(!newStudent){
+        throw new Error("failed to create a student ")
+
+      }
+
+
+      await session.commitTransaction()
+      await session.endSession()
+
+      return newStudent;
+    }
+  
+ catch (error) {
+  await session.abortTransaction()
+  await session.endSession()
+
+   
+  
+}
 };
 
 export const UserServices = {
